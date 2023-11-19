d6cf0140b47946416edfcfa398b1b31e5219f34d
Eugen <krizhik@gmail.com>
2023-11-19 19:20:18 +0300

    idea fix

---
+++ build/src_components_About_About_tsx.076125dc1c5f9161eee1.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunkprod_learning"] = self["webpackChunkprod_learning"] || []).push([["src_components_About_About_tsx"],{
+
+/***/ "./src/components/About/About.tsx":
+/*!****************************************!*\
+  !*** ./src/components/About/About.tsx ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var About = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "About component" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (About);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX2NvbXBvbmVudHNfQWJvdXRfQWJvdXRfdHN4LjA3NjEyNWRjMWM1ZjkxNjFlZWUxLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLDZCQUE2QjtBQUN2RDtBQUNBLGlFQUFlLEtBQUssRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2RfbGVhcm5pbmcvLi9zcmMvY29tcG9uZW50cy9BYm91dC9BYm91dC50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsganN4IGFzIF9qc3ggfSBmcm9tIFwicmVhY3QvanN4LXJ1bnRpbWVcIjtcbnZhciBBYm91dCA9IGZ1bmN0aW9uICgpIHtcbiAgICByZXR1cm4gKF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogXCJBYm91dCBjb21wb25lbnRcIiB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXQ7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
