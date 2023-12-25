be6c6bbfe9c46703f5bd48b2909fb6230f3d5e80
Eugen <krizhik@gmail.com>
2023-12-25 18:11:36 +0300

    21: unknown paths, loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,18 +1,22 @@
 import { RouteProps } from 'react-router-dom';
 import Main from 'pages/Main';
 import About from 'pages/About';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'main',
-    ABOUT = 'about'
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: RouteProps[] = [
     { path: RoutePath[AppRoutes.MAIN], element: <Main /> },
     { path: RoutePath[AppRoutes.ABOUT], element: <About /> },
+    { path: RoutePath[AppRoutes.NOT_FOUND], element: <NotFoundPage /> },
 ];
