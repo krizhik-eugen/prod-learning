21feb44d6f77e7c3617b1b220c3bb47a8782ef51
Eugen Krizhik <eugen@Eugens-MacBook-Air.local>
2024-01-07 13:20:31 +0300

    fix for translation error

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,27 +1,31 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(styles.Navbar, {}, [className])}>
-        <div className={styles.links}>
-            <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/"
-                className={styles.mainLink}
-            >
-                Main
-            </AppLink>
-            <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/about"
-            >
-                About
-            </AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(styles.Navbar, {}, [className])}>
+            <div className={styles.links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/"
+                    className={styles.mainLink}
+                >
+                    { t('Main page')}
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/about"
+                >
+                    {t('About')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
