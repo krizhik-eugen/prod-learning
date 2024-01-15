234a05e1595e1359926e225a1687d4a1d30cf565
Eugen Krizhik <eugen@Eugens-MacBook-Air.local>
2024-01-15 18:14:19 +0300

    27: added translations for toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LanguageSwitcher } from 'widgets/LanguageSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggleCollapsed = () => {
         setCollapsed((prevState) => !prevState);
@@ -23,12 +25,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 [styles.collapsed]: collapsed,
             }, [className])}
         >
-            {/* <Button */}
-            {/*    className={styles.collapsed} */}
-            {/*    onClick={onToggleCollapsed} */}
-            {/* > */}
-            {/*    toggle */}
-            {/* </Button> */}
+            <Button
+                className={styles.collapsed}
+                onClick={onToggleCollapsed}
+            >
+                { t('Toggle') }
+            </Button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
                 <LanguageSwitcher className={styles.langSwitcher} />
