80ee1a28d17a74cb755294ab64a339628e15fcf9
Eugen Krizhik <eugen@Eugens-MacBook-Air.local>
2024-03-03 17:25:43 +0300

    35: async reducer, update tests

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore, StateSchema } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
     children: ReactNode;
-    initialState?: DeepPartial<StateSchema>
+    initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
-export const StoreProvider = ({ children, initialState }: StoreProviderProps) => {
-    const store = createReduxStore(initialState as StateSchema);
+export const StoreProvider = ({ children, initialState, asyncReducers }: StoreProviderProps) => {
+    const store = createReduxStore(
+        initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
 
     return (
         <Provider store={store}>
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,11 +1,19 @@
 import '../../../../app/styles/index.scss';
 import { Story } from '@storybook/react';
 import React from 'react';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import { StateSchema, StoreProvider } from '../../../../app/providers/StoreProvider';
 
-const StoreDecorator = (state: DeepPartial<StateSchema>) => (Story: Story) => (
-    <StoreProvider initialState={state}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
+
+const StoreDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
+) => (Story: Story) => (
+    <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
         <Story />
     </StoreProvider>
 );
